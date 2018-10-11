cb871fd908597373668364ccf1563f4109b833e3
Antonio Ribeiro <antonioribeiro.rec@gmail.com>
2018-10-11 19:46:21 -0300

    implementacao somar

---
+++ src/Calculadora.java
@@ -0,0 +1,7 @@
+
+public class Calculadora {
+
+	public static int somar(int n1, int n2) {
+		return n1 + n2;
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Alo Mundo");
+		System.out.println(Calculadora.somar(1, 2));
 	}
 }
